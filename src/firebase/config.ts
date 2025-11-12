3c483f27f672cfbdba3cebb1e09a789521fa77ce
MOHAMMED RADI <mohammedradi651@gmail.com>
2025-11-12 21:16:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9989987135-55b79",
+  "appId": "1:707864263666:web:839a09390e62816de8496b",
+  "apiKey": "AIzaSyDlL5va6QgZB4A6WO5ouklnGE-6b6PfDVM",
+  "authDomain": "studio-9989987135-55b79.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "707864263666"
+};
