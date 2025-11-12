694090c3dbb8bb73862ab8f74fd11d670efeac3a
MOHAMMED RADI <mohammedradi651@gmail.com>
2025-11-12 20:05:36 +0000

    To disable this UI completely, set devIndicators: false in your next.con

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: {
-    buildActivity: false,
-  },
+  devIndicators: false,
   typescript: {
     ignoreBuildErrors: true,
   },
