1aa0dd1ee54b7113a54595a23b6f0f1bf313fa78
MOHAMMED RADI <mohammedradi651@gmail.com>
2025-11-13 14:33:58 +0000

    يعني مثلا هذا الخط Tajawal في الجوال خط مش مناسب يختلف وفي الكمبيوتر خط

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Almarai:wght@400;700&family=Cairo:wght@400;700&family=Tajawal:wght@400;700&family=IBM+Plex+Sans+Arabic:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Almarai:wght@400;700&family=Cairo:wght@400;700&family=Tajawal:wght@200;300;400;500;700;800;900&family=IBM+Plex+Sans+Arabic:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </head>
