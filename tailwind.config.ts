e46b69329cb27f13f78ca47ac8c0b787c7b9b737
MOHAMMED RADI <mohammedradi651@gmail.com>
2025-11-15 03:29:54 +0000

    في الصفحة الرئيسية وحسابي يظهر وميض 2 وميض احذف الاخضر

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -48,7 +48,7 @@ export default {
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
         chart: {
-          '1': 'hsl(var(--chart-1))',
+          '1': 'hsl(var(--primary))',
           '2': 'hsl(var(--chart-2))',
           '3': 'hsl(var(--chart-3))',
           '4': 'hsl(var(--chart-4))',
