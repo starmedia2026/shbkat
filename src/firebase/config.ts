d12e0f4665cdbaddf7208dfaa482cbbea2947163
MOHAMMED RADI <mohammedradi651@gmail.com>
2025-11-15 11:48:03 +0000

    const firebaseConfig = {   apiKey: "xxxx",   authDomain: "project-id.fir

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-9989987135-55b79",
-  "appId": "1:707864263666:web:839a09390e62816de8496b",
-  "apiKey": "AIzaSyDlL5va6QgZB4A6WO5ouklnGE-6b6PfDVM",
-  "authDomain": "studio-9989987135-55b79.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "707864263666"
+  apiKey: "xxxx",
+  authDomain: "project-id.firebaseapp.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "...",
+  appId: "..."
 };
